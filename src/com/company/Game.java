d569e13ffed0acb12ca10052b9a4f67435546726
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-11-07 22:21:38 +0600

    formatting code, rename some variables

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -6,15 +6,17 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
-
-    public void filmsHistogram(String films[]){
-        //System.out.println("title\t\t\tnumber of symbols\tHistogram"); //need to print it out depends on quantity of symbols
-        for (String i: films) {
+    private Random random = new Random(); 
+    public void filmsHistogram(String films[]) {
+        // System.out.println("title\t\t\tnumber of symbols\tHistogram"); //need to
+        // print it out depends on quantity of symbols
+        for (String i : films) {
             String film = i;
-            //System.out.printf("\n%12s %5d \t\t\t\t\t", i, i.length() ); ////need to print it out depends on quantity of symbols
-            System.out.print("\n" +  i + " " +  i.length() + " " );
-            for ( int j = 0; j < film.length(); j++){
-                if (j == film.length()-1){
+            // System.out.printf("\n%12s %5d \t\t\t\t\t", i, i.length() ); ////need to print
+            // it out depends on quantity of symbols
+            System.out.print("\n" + i + " " + i.length() + " ");
+            for (int j = 0; j < film.length(); j++) {
+                if (j == film.length() - 1) {
                     System.out.print("_");
                     continue;
                 }
@@ -23,27 +25,22 @@ public class Game {
         }
     }
 
-    public void printFilms(String films[]){
+    public void printFilms(String films[]) {
         System.out.println("\n");
-        for (String i: films) {
+        for (String i : films) {
             System.out.println(i);
         }
 
     }
 
-    public boolean openString(String s){
+    public boolean openString(String s) {
         if (s.isEmpty())
             return false;
         else
             return true;
     }
 
-    public  String wordToUnderscore(String word){
-        //old version
-/*        for (int i = 0; i < word.length(); i++) {
-            word = word.replace(word.charAt(i), '_');
-        }*/
-        //new version to handle the spaces
+    public String wordToUnderscore(String word) {
         StringBuilder film = new StringBuilder(word);
         for (int i = 0; i < film.length(); i++) {
             if (film.charAt(i) == ' ')
@@ -54,40 +51,38 @@ public class Game {
         return word;
     }
 
-    public String pickRandomFilm(String films[]){
-        Random f = new Random();
-        String film = films[f.nextInt(films.length)];
-        return film;
+    public String pickRandomFilm(String films[]) {
+        return films[random.nextInt(films.length)];
+
     }
 
-    public void guessTheFilmGame(String film){
+    public void guessTheFilmGame(String film) {
         String originalFilm = film;
         char[] wrongLetters = new char[30];
         int wrongGuesses = 0, tries = 0;
 
-        //System.out.println(film);
+        // System.out.println(film);
         film = wordToUnderscore(film);
-        //System.out.println(film);
+        // System.out.println(film);
 
-        Scanner s = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         StringBuilder tempFilm = new StringBuilder(film);
-        //StringBuffer tempFilm = new StringBuffer(thisFilm); //used originally
+        // StringBuffer tempFilm = new StringBuffer(thisFilm); //used originally
         System.out.println("\n\n\t\t\t*** LET THE FILM GUESSING GAME BEGIN!!! ***\n");
         System.out.print("enter the letter: ");
-        String c = s.nextLine();
+        String c = scanner.nextLine();
         char q = c.charAt(0);
 
         while (true) {
             tries++;
             if (originalFilm.indexOf(q) == -1) {
                 wrongGuesses++;
-                //System.out.println("no " + q + " in " + originalFilm);
-                System.out.println("Title of the film does not contain the letter \"" + q + "\"" );
-
+                // System.out.println("no " + q + " in " + originalFilm);
+                System.out.println("Title of the film does not contain the letter \"" + q + "\"");
 
                 boolean b = true;
-                for (int i = 0; i < wrongLetters.length; i++){
-                    if(wrongLetters[i] == q ) {
+                for (int i = 0; i < wrongLetters.length; i++) {
+                    if (wrongLetters[i] == q) {
                         System.out.println("you have already entered this letter: " + q);
                         --wrongGuesses;
                         b = false;
@@ -96,15 +91,15 @@ public class Game {
                 }
 
                 if (b)
-                    wrongLetters[wrongGuesses-1] = q;
+                    wrongLetters[wrongGuesses - 1] = q;
 
                 System.out.print("wrong letters: ");
-                for (int i = 0; i < wrongLetters.length; i++){
+                for (int i = 0; i < wrongLetters.length; i++) {
                     System.out.print(wrongLetters[i] + " ");
                 }
 
-            } else  {
-                byte printOnce = 0 ;
+            } else {
+                byte printOnce = 0;
                 for (int i = 0; i < tempFilm.length(); i++) {
                     if (originalFilm.charAt(i) == q) {
                         printOnce++;
@@ -129,20 +124,19 @@ public class Game {
             System.out.println(tempFilm);
 
             System.out.print("\nenter the letter: ");
-            c = s.nextLine();
+            c = scanner.nextLine();
             q = c.charAt(0);
 
         }
     }
 
-    public boolean isFileCanBeOpened(){
+    public boolean isFileCanBeOpened() {
         try {
             File file = new File("films.txt");
             Scanner scanner = new Scanner(file);
             System.out.println("\n\nfile is opened!");
             return true;
-        }
-        catch(FileNotFoundException exception) {
+        } catch (FileNotFoundException exception) {
             // Handle the situation by letting the user know what happened
             System.out.println("Cannot find this file!");
             return false;
@@ -151,20 +145,22 @@ public class Game {
 
     public String[] readFromFile() throws FileNotFoundException {
 
-            File file = new File("films.txt");
-            Scanner scanner = new Scanner(file);
-            int numberOfFilms = howManyFilmsInFile();
-            String [] films = new String[numberOfFilms];
-            int i = 0;
-            while (scanner.hasNextLine()) { //return true or false
-                String line = scanner.nextLine(); //return every string
-                films[i] = line;
-                i++;
+        File file = new File("films.txt");
+        Scanner scanner = new Scanner(file);
+        int numberOfFilms = howManyFilmsInFile();
+        String[] films = new String[numberOfFilms];
+        int i = 0;
+        while (scanner.hasNextLine()) { // return true or false
+            String line = scanner.nextLine(); // return every string
+            films[i] = line;
+            i++;
 
-            //printFilms(films);
-/*            for (i=0; i<films.length; i++){
-                System.out.println(i+1 + ". " + films[i]);
-            }*/
+            // printFilms(films);
+            /*
+             * for (i=0; i<films.length; i++){
+             * System.out.println(i+1 + ". " + films[i]);
+             * }
+             */
         }
         return films;
     }
@@ -173,13 +169,10 @@ public class Game {
         File file = new File("films.txt");
         Scanner scanner = new Scanner(file);
         int films = 0;
-        while (scanner.hasNextLine()) { //return true or false
-            String line = scanner.nextLine(); //return every string
+        while (scanner.hasNextLine()) { // returns true or false
+            String line = scanner.nextLine(); // returns every string
             films += line.split("\n").length;
         }
         return films;
     }
 }
-
-
-
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,31 +1,27 @@
 package com.company;
+
 import java.io.FileNotFoundException;
 
 public class Main {
-    static String[] films = {"interstellar", "terminator", "predator", "ameli"};
+    static String[] films = { "interstellar", "terminator", "predator", "ameli" };
 
     public static void main(String[] args) throws FileNotFoundException {
 
-
         Game game = new Game();
 
-        //game.filmsHistogram(films); //films from here
+        // game.filmsHistogram(films); //films from here
 
+        System.out.println(game.isFileCanBeOpened()); // check if file is open
 
-        System.out.println(game.isFileCanBeOpened()); //check if file is open
+        String[] movies = game.readFromFile(); // films from file
 
-        String[] Movies = game.readFromFile(); //films from file
+        game.printFilms(movies); // print out the list of films from file
 
-        game.printFilms(Movies); //print out the list of films from file
+        game.filmsHistogram(movies);
 
-        game.filmsHistogram(Movies);
+        String xFilm = game.pickRandomFilm(movies); // picking random films from Movies list
 
-        String xFilm = game.pickRandomFilm(Movies); //picking random films from Movies list
-
-        game.guessTheFilmGame(xFilm); //the guessing game
-
-
-
+        game.guessTheFilmGame(xFilm); // the guessing game
 
     }
 }
